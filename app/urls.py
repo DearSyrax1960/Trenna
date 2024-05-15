adf919b2b2dc7014afe178c4e93f5ac53a9878db
Himeno <justforsocialmediii@gmail.com>
2024-05-15 22:17:25 +0330

    changed urls app from terenna to trenna.

--- app/urls.py
+++ app/urls.py
@@ -24,5 +24,5 @@ admin.site.index_title = 'Admin'
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("__debug__/", include(debug_toolbar.urls)),
-    path('Terenna/', include('core.urls'))
+    path('Trenna/', include('core.urls'))
 ]
