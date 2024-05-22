20d5904bfb701317f5116f2b845ea0096191f694
Himeno <justforsocialmediii@gmail.com>
2024-05-22 13:15:27 +0330

    getting id from urls for delete and create an instance.

--- core/serializers.py
+++ core/serializers.py
@@ -12,12 +12,18 @@ class WorkspaceSerializer(serializers.ModelSerializer):
 class BoardSerializer(serializers.ModelSerializer):
     class Meta:
         model = Board
-        fields = ['id', 'title', 'workspace']
-        workspace = WorkspaceSerializer()
+        fields = ['id', 'title']
+
+    def create(self, validated_data):
+        workspace_id = self.context['workspace_id']
+        return Board.objects.create(workspace_id=workspace_id, **validated_data)
 
 
 class TaskSerializer(serializers.ModelSerializer):
     class Meta:
         model = Task
-        fields = ['id', 'description', 'board']
-        board = BoardSerializer()
+        fields = ['id', 'description']
+
+    def create(self, validated_data):
+        board_id = self.context['board_id']
+        return Task.objects.create(board_id=board_id, **validated_data)
--- core/views.py
+++ core/views.py
@@ -9,13 +9,23 @@ from .serializers import TaskSerializer, WorkspaceSerializer, BoardSerializer
 
 
 class TaskViewSet(ModelViewSet):
-    queryset = Task.objects.all()
     serializer_class = TaskSerializer
+
+    def get_queryset(self):
+        return Task.objects.filter(board_id=self.kwargs['board_pk'])
+
+    def get_serializer_context(self):
+        return {'board_id': self.kwargs['board_pk']}
 
 
 class BoardViewSet(ModelViewSet):
-    queryset = Board.objects.all()
     serializer_class = BoardSerializer
+
+    def get_queryset(self):
+        return Board.objects.filter(workspace_id=self.kwargs['workspace_pk'])
+
+    def get_serializer_context(self):
+        return {'workspace_id': self.kwargs['workspace_pk']}
 
 
 class WorkspaceViewSet(ModelViewSet):
