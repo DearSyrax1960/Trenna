56ed4726da80d264eadf4c567a73ef61011a3b89
Himeno <justforsocialmediii@gmail.com>
2024-05-25 22:11:25 +0330

    user model edited.

--- core/models.py
+++ core/models.py
@@ -1,8 +1,11 @@
 from django.db import models
 
+from app import settings
+
 
 class Workspace(models.Model):
-    # owner or members
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="workspace")
     title = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
