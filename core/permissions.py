744fadca228aca434afc4c8657634f7e01adbec1
Himeno <justforsocialmediii@gmail.com>
2024-05-26 23:17:35 +0330

    solved permission problems.

---
+++ core/permissions.py
@@ -0,0 +1,41 @@
+from django.db.models import Q
+from rest_framework import permissions
+from .models import *
+
+
+class IsWorkspaceOwner(permissions.BasePermission):
+    def has_permission(self, request, view):
+        owner_id = request.user.id
+        workspace_id = view.get_kwargs()['workspace_pk'] or view.get_kwargs['pk']
+        return Workspace.objects.filter(
+            Q(id=workspace_id) & Q(owner_id=owner_id)
+        ).exists()
+
+
+class IsBoardOwner(permissions.BasePermission):
+    def has_permission(self, request, view):
+        owner_id = request.user.id
+        workspace_id = view.get_kwargs()['workspace_pk']
+        board_id = view.get_kwargs()['pk']
+
+        return Workspace.objects.filter(
+            Q(id=workspace_id) & Q(owner_id=owner_id)
+        ).exists() & Board.objects.filter(
+            Q(id=board_id) & Q(workspace_id=workspace_id)
+        ).exists()
+
+
+class IsTaskOwner(permissions.BasePermission):
+    def has_permission(self, request, view):
+        owner_id = request.user.id
+        workspace_id = view.get_kwargs()['workspace_pk']
+        board_id = view.get_kwargs()['board_pk']
+        task_id = view.get_kwargs()['pk']
+
+        return Workspace.objects.filter(
+            Q(id=workspace_id) & Q(owner_id=owner_id)
+        ).exists() & Board.objects.filter(
+            Q(id=board_id) & Q(workspace_id=workspace_id)
+        ).exists() & Task.objects.filter(
+            Q(id=task_id) & Q(board_id=board_id)
+        ).exists()
--- core/views.py
+++ core/views.py
@@ -1,7 +1,8 @@
-
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
 
 from .models import *
+from .permissions import IsWorkspaceOwner, IsBoardOwner, IsTaskOwner
 from .serializers import TaskSerializer, WorkspaceSerializer, BoardSerializer
 
 
@@ -17,6 +18,15 @@ class TaskViewSet(ModelViewSet):
     def get_serializer_context(self):
         return {'board_id': self.kwargs['board_pk']}
 
+    def get_kwargs(self):
+        return self.kwargs
+
+    def get_permissions(self):
+        if self.request.method in ['PATCH', 'DELETE', 'PUT']:
+            return [IsTaskOwner()]
+        # post get ha
+        return [IsBoardOwner()]
+
 
 class BoardViewSet(ModelViewSet):
     serializer_class = BoardSerializer
@@ -27,13 +37,29 @@ class BoardViewSet(ModelViewSet):
     def get_serializer_context(self):
         return {'workspace_id': self.kwargs['workspace_pk']}
 
+    def get_kwargs(self):
+        return self.kwargs
+
+    def get_permissions(self):
+        if self.request.method in ['PATCH', 'DELETE', 'PUT']:
+            return [IsBoardOwner()]
+        # post get ha
+        return [IsWorkspaceOwner()]
+
 
 class WorkspaceViewSet(ModelViewSet):
     serializer_class = WorkspaceSerializer
 
     def get_queryset(self):
         return Workspace.objects.filter(owner_id=self.request.user.id)
-    # select_related ???/
 
     def get_serializer_context(self):
         return {'owner_id': self.request.user.id}
+
+    def get_permissions(self):
+        if self.request.method in ['PATCH', 'DELETE', 'GET', 'PUT']:
+            return [IsWorkspaceOwner()]
+        return [IsAuthenticated()]
+
+    def get_kwargs(self):
+        return self.kwargs
