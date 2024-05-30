a1be44e4078a992dec04f9fb53c6d0bcc178bd96
Himeno <justforsocialmediii@gmail.com>
2024-05-30 11:30:33 +0330

    deleted slug field.

--- core/models.py
+++ core/models.py
@@ -7,7 +7,7 @@ class Workspace(models.Model):
     owner = models.ForeignKey(
         settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="workspace")
     title = models.CharField(max_length=255)
-    slug = models.SlugField(max_length=255, allow_unicode=True, unique=True, blank=True)
+    # slug = models.SlugField(max_length=255, allow_unicode=True, unique=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
@@ -17,7 +17,7 @@ class Workspace(models.Model):
 
 class Board(models.Model):
     title = models.CharField(max_length=255)
-    slug = models.SlugField(max_length=255, allow_unicode=True, unique=True, blank=True)
+    # slug = models.SlugField(max_length=255, allow_unicode=True, unique=True, blank=True)
     workspace = models.ForeignKey(Workspace, on_delete=models.CASCADE, related_name="board")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
