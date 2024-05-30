95298b44934ff26ee56b376fefb867c01713790b
Himeno <justforsocialmediii@gmail.com>
2024-05-30 19:06:10 +0330

    added some fields to perform reminder option.

--- core/models.py
+++ core/models.py
@@ -27,7 +27,17 @@ class Board(models.Model):
 
 
 class Task(models.Model):
+    DONE = 'd'
+    WORKING = 'w'
+    PENDING = 'p'
+    STATUS_CHOICES = (
+        (DONE, 'Done'),
+        (WORKING, 'Working on it'),
+        (PENDING, 'pending')
+    )
     description = models.TextField(null=False, blank=False)
+    status = models.CharField(max_length=1, choices=STATUS_CHOICES, default=PENDING)
+    expire_at = models.DateField(null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     board = models.ForeignKey(Board, on_delete=models.CASCADE, related_name="task")
